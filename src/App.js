ac24b215efcd6699ccfb8768a0f2374f8c595aed
Alexis Leonardo Jalomo <ajalom2@uic.edu>
2022-03-03 23:30:19 -0600

    NavBar version 2 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import NavBar from './NavBar'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/NavBar.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled, { css } from 'styled-components'
+
+const UnorderedList = styled.ul`
+    list-style-type: none;
+    display: flex;
+    padding: 18px 0;
+    background-size: 400% 400%;
+    background-image: linear-gradient(-45deg, #ff5959, #ff4040, #ff0d6e, #ff8033,#d74177);
+    animation: 12s myGradient infinite;
+    border-radius: 40px;
+    box-shadow: 3px 3px 20px #ff3352;
+    border-top-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+
+    @keyframes myGradient {
+        0% {
+          background-position: 0% 50%;
+        }
+        
+        50%{
+          background-position: 100% 50%;
+        }
+        
+        100% {
+          background-position: 0% 50%;
+        }
+    }
+`
+
+const ListItem = styled.li`
+    a {
+        color: #fff;
+        text-decoration: none;
+        padding: 0px 45px;
+        font-family: verdana;
+        font-weight: lighter;
+        font-size: 24px;
+        letter-spacing: 1px;
+        opacity: 0.9;
+    }
+
+    a:hover {
+        color: #fff;
+        animation: 1.5s myFlash linear infinite;
+        text-shadow: 2px 2px 30px #fff;
+        transition: 2s;
+    }
+    
+
+    @keyframes myFlash {
+        0% {
+          opacity: 1;
+          text-shadow: transparent;
+        }
+        
+        50% {
+          opacity: .1;
+        }
+        
+        100% {
+          opacity: 1;
+          text-shadow: 2px 2px 20px #fff;
+        }
+      }
+`
+
+const Container = styled.div`
+    box-sizing: border-box;
+    margin: 0;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 6%;
+    left: 50%;
+`
+
+function NavBar() {
+return (
+    <Container>
+    <UnorderedList>
+        <ListItem> <a href=''>About</a> </ListItem>
+        <ListItem> <a href=''> Resume</a> </ListItem>
+        <ListItem> <a href=''>Experience</a> </ListItem>
+        <ListItem> <a href=''>More</a> </ListItem>
+    </UnorderedList>
+  </Container>
+)
+}
+
+export default NavBar;
